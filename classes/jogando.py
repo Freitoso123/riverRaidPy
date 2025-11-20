c31152542e33a01518e9841a40736a465c4eb2fc
Nicolas de Freitas Silva <nicolas.defreytas@gmail.com>
2025-11-20 11:03:28 -0300

    comentários funcioando os mapas

--- classes/jogando.py
+++ classes/jogando.py
@@ -1,6 +1,7 @@
 import pygame, random, sys, time
 from classes.aviao import Aviao
 from classes.helic import *
+from classes.mapa import *
 
 #criei um função separada para a lógica do jogo por conta da administração de estados do jogo, para que fosse possivel
 #recomeçar o jogo
@@ -22,54 +23,91 @@ def Jogando(dif):
     balas = []
     reload_time = 0.25
     lastShot = 0
+
+    map_list = []
+    first_map = True
+
+    col_player = False
     
     while True:
 
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
-                sys.exit()
+                #retorna sair para o meu controle de estados identificar a saida
+                return "sair"
 
         gameDisplay.fill("royalblue4")
         tempo.tick(30)
 
         tecla = pygame.key.get_pressed()
-        SETACIMA = pygame.key.get_pressed()
-
-        player.imprimir()
-        player.movPlayer(tecla)
 
         #gerar bala
         if tecla[pygame.K_SPACE] and (time.time() - lastShot) >= reload_time:
             balas.append(player.atirar())
             lastShot = time.time()
 
-        if random.randint(0,100) > 1:
+        #gerar helicopteros
+        if random.randint(0,1000) > 330 and len(helic_list) < 10:
             #aquie eu recebo a dificuldade selecionada
             helic_list.append(gerarhelic(dif))
-        
-        #imprime as entidades em vetores
+
+        #adiciona 1o modulo a lista de mapas
+        if first_map:
+            map_list.append(gerarMapa())
+            first_map = False
+
+        #for das entidades
+        for module in map_list:
+            module.imprimir()
+            module.queda(tecla)
+            
+            #apagar modulo que passa da tela
+            if module.y > 960:
+                map_list.remove(module)
+
+            #gerar modulo
+            if module.y > 0 and len(map_list) < 2:
+                map_list.append(gerarMapa())
+                newMapTime = 0
+
+            #gameOver
+            if module.colisaoMask(player):
+                return
+
         for bala in balas:
             bala.imprimir()
             bala.movTiro()
+
+            #apagar bala fora da tela
             if bala.y < -32:
                 balas.remove(bala)
         
         for helic in helic_list:
             helic.imprimir()
             helic.movHoriz()
-            helic.queda(SETACIMA)
-            for bala in balas:
-                col = helic.colisaoRect(bala)
-                if col == True:
-                    helic_list.remove(helic)
-                    balas.remove(bala)
+            helic.queda(tecla)
+
+            #apagar helicopteros que passaram da tela
             if helic.y > 832:
                 helic_list.remove(helic)
-            if helic.y < -50:
-                helic_list.remove(helic)
 
-            col_player = helic.colisaoMask(player)
-            if col_player == True:
-                #não retorna nada pois o proximo estado é sempre game_over
-                return
+            #gameOver
+            if helic.colisaoMask(player):
+                col_player = True
+
+            #apagar helicopteros acertador por balas
+            for bala in balas:
+                helic_hitted = helic.colisaoRect(bala)
+                if helic_hitted == True:
+                    helic_list.remove(helic)
+                    balas.remove(bala)
+
+        #gameOver
+        if col_player == True:
+            #não retorna nada pois o meu próximo estado é game_over
+            return
+
+        player.imprimir()
+        player.movPlayer(tecla)
+
         pygame.display.update()
